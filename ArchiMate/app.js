e1c771552b11f8825a151bbebf5ef3f9d34f7c5a
gbruneau <guy.bruneau@gmail.com>
2021-12-13 16:38:46 -0500

    Add files via upload

--- ArchiMate/app.js
+++ ArchiMate/app.js
@@ -1,49 +1,58 @@
 $(function () {
-    $("#layers").accordion({
-        heightStyle: "content"
-    });
-});
-
-$(function () {
-    $(".gbTabs").tabs();
 
     $(function () {
-        $(document).tooltip({
-            track: true,
-            position: { my: "right-50 bottom", at: "left bottom", collision: "flipfit" }
+        $("#layers").accordion({
+            heightStyle: "content",
+            collapsible: true
         });
     });
 
-});
+    $(function () {
+        $(".gbTabs").tabs();
 
-$(function () {
-    // Dialog Caracteristics
-    $("#diagRef").dialog(
-        {
-            autoOpen: false,
-            minWidth: 700,
-            position: {
-                my: "top",
-                at: "center top"
-            } ,
-            hide: 'explode',
-            show: 'puff'
-        }
-    );
-    // On click event         
+        $(function () {
+            $(document).tooltip({
+                track: true,
+                position: { my: "right-50 bottom", at: "left bottom", collision: "flipfit" }
+            });
+        });
 
-    $(".arcExample").click(function () {
-        $("#diagRef").dialog("open");
     });
 
+    /* === Dialog box functions for references ===*/
+
+    $(function () {
+        // Dialog Caracteristics
+        $(".gbDialog").dialog(
+            {
+                autoOpen: false,
+                minWidth: 500,
+                position: {
+                    my: "top",
+                    at: "center top"
+                },
+                hide: { effect: 'fade', duration: 500 },
+                show: { effect: 'fade', duration: 500 },
+                duration: 50
+            }
+        );
+        // On click event         
+
+        $(".gbHelp").click(function () {
+            $("#diagRef").dialog("open");
+        });
+    });
+
+    /*   Add Zoom  */
+
+    var newImg;
+    $(".arcExample").each(function () {
+        src=$(this).attr('src');
+        newImg = '<div class="container"><div class="zoom"><img  src=' + src + '></div></div>';
+        $(newImg).insertAfter(this);
+        console.log(newImg);
+        $(this).css('display', 'none');
+    })
+    
+    zoom();
 });
-
-function loadHtml(aSelection,url) {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function () {
-        $(aSelection).append(this.responseText);
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
-
